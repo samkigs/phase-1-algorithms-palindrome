eb1a5e9e0c675cab29d7a3e6ca66e8689803b066
samkigs <samuel.kihuga@student.moringaschool.com>
2022-06-20 21:59:32 +0300

    algorithms

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+function isPalindrome(string) {
+  //   // Write your algorithm here
+  //   //program that checks whether word is a palindrome or not, ie the reverse of the string is same as the original
+    
+        let revString= string.split("").reverse().join("")
+         console.log(revString)
+  
+        if (revString=== string){
+         return true
+            } else{
+         return false
+          } 
+  }
+   isPalindrome("motto")
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+  if (require.main === module) {
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+  
+  module.exports = isPalindrome;
